0fa6235a06473b709a612a6596dba10d41f0ed57
Amanda Southworth <amanda.southworth@myds.us>
2016-12-20 21:24:41 -0800

    added fileScope.cpp

---
+++ code/scopeAndStuff.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+
+//talking about scopes!
+
+int multiplyThese(int x, int y) {
+    
+    return x * y;
+}
+
+int main() {
+   
+    
+    
+    return 0; 
+}
