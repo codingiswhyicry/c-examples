8369641402b7a1072a8d19b960fa7e123dce1404
Amanda Southworth <amanda.southworth@myds.us>
2016-12-20 21:50:39 -0800

    added scopeAndStuff executable

--- code/scopeAndStuff.cpp
+++ code/scopeAndStuff.cpp
@@ -3,14 +3,31 @@ using namespace std;
 
 //talking about scopes!
 
-int multiplyThese(int x, int y) {
+int multiplyThese(int x, int y) {  //integers x and y created within local scope
     
-    return x * y;
-}
+    return x * y; //integers x and y are used
+    
+} //integers x and y are destroyed within the scope
 
 int main() {
    
-    
+  cout << "input two integers!" << endl; 
+  
+  //creates integer a and b within the scope
+  int a; 
+  int b; 
+  
+  //asks for user input about integer a  
+  cout << "input integer a!" << endl;
+  cin >> a; 
+  
+  //asks for user input about integer b 
+  cout << "input integer b!" << endl;
+  cin >> b; 
+   
+  //takes the two and multiply them
+  cout << "here are these two multiplied together!" << endl; 
+  cout << multiplyThese(a, b) << endl; 
     
     return 0; 
 }
