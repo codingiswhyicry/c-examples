68f84b121c313544499cabff2cff519fa448654d
Amanda Southworth <amanda.southworth@myds.us>
2016-12-21 18:48:42 -0800

    added literals and operatoes

---
+++ code/literalsAndOperators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+//literals and operators
+
+int main() {
+    
+    int x = 4; //x is a variable, and 4 is the literal. The literal is a hard coded and embedded value
+    
+    cout << 5 + 8 << endl; //5 + 8 is an expression, and 5 and 8 are the literals
+    
+    int y = 5 + 6; 
+    // +. -, *, /, and == are operators. An operators tell an expression how to combine variables and literals
+    
+
+   return 0;
+}
