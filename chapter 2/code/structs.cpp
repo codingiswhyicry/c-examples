f7a7f628e62d7a1e6119cd216032cde38fbfc416
Amanda Southworth <amanda.southworth@myds.us>
2017-01-04 19:08:56 -0800

    added changes to the struct.cpp file

---
+++ chapter 2/code/structs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std; 
+
+struct drinks {
+    
+    int drinkTemp; 
+    string drinkName; 
+    double drinkCost;    
+    
+}
+
+int getTemp(void) {
+    
+    
+    
+}
+
+string getName(void) {
+    
+    
+}
+
+double getCost(void) {
+    
+}
+
+int main() {
+    
+    
+    return 0;
+}
