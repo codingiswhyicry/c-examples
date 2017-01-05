747f5aba34cafdd63fce43c6e5674bfc432160a8
Amanda Southworth <amanda.southworth@myds.us>
2017-01-04 19:12:16 -0800

    added function getCost

--- chapter 2/code/structs.cpp
+++ chapter 2/code/structs.cpp
@@ -16,19 +16,22 @@ int getTemp(void) {
     cout << "What's the temperater you want your drink?" << endl; 
     
     int temperature; 
-    
     cin >> temperature; 
-    
     return temperature;
     
 }
 
 string getName(void) {
     
-    
+    string name; 
+    cout << "What's the name of your drink?" << endl; 
+    cin << name; 
+    return name;
 }
 
 double getCost(void) {
+    
+    
     
 }
 
